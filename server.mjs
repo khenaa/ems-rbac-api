3bbad891adfa180e040dee99d46a77f8689238e5
khenaa <ilenikhenajnr@gmail.com>
2024-12-09 16:15:43 +0100

    code cleanup in server.mjs and createAdminScript

--- server.mjs
+++ server.mjs
@@ -1,31 +1,6 @@
-// import mongoose from 'mongoose';
-// import app from './index.mjs';
-
-// mongoose
-//   .connect(process.env.DB_URI, {
-//     serverSelectionTimeoutMS: 60000,
-//   })
-//   .then(() => {
-//     console.log('DB connection successful 🤝');
-//   })
-//   .catch((err) => {
-//     console.log(err);
-//   });
-
-// const PORT = process.env.PORT || 3000;
-
-// app.listen(PORT, () => {
-//   console.log(`listening on port ${PORT}....`);
-// });
-
-// export { mongoose };
-// export default app;
-
-// server.mjs
 import mongoose from 'mongoose';
 import app from './index.mjs';
 
-// Mongoose connection setup
 mongoose
   .connect(process.env.DB_URI, {
     serverSelectionTimeoutMS: 60000,
@@ -43,6 +18,6 @@ app.listen(PORT, () => {
   console.log(`Server is listening on port ${PORT}....`);
 });
 
-// Export mongoose to be used in other files
+// export mongoose to be used in other files
 export { mongoose };
 export default app;
